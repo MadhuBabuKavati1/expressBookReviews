d9f6697d29b54a6e9d2ebfba353f583a4ce7759c
MadhuBabuKavati1 <162259846+MadhuBabuKavati1@users.noreply.github.com>
2024-07-29 01:18:27 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
@@ -8,15 +8,32 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+  secret: "fingerprint_customer",
+  resave: true,
+  saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+// Authentication middleware
+app.use("/customer/auth/*", function auth(req, res, next) {
+  const token = req.headers['authorization']?.split(' ')[1]; // Bearer <token>
+  
+  if (!token) {
+    return res.status(403).json({ message: "Token required" });
+  }
+
+  jwt.verify(token, "your_jwt_secret", (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
 });
- 
-const PORT =5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
